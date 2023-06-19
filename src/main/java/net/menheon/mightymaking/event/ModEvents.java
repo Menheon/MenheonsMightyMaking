4fc94b9c493063a67bbe82916546e893b9a19f3c
Menheon <kasper.d.borg@gmail.com>
2023-06-19 17:26:27 +0200

    Added spawn rules for EnderGhoulEntity

--- src/main/java/net/menheon/mightymaking/event/ModEvents.java
+++ src/main/java/net/menheon/mightymaking/event/ModEvents.java
@@ -3,7 +3,11 @@ package net.menheon.mightymaking.event;
 import net.menheon.mightymaking.MightyMaking;
 import net.menheon.mightymaking.entity.ModEntities;
 import net.menheon.mightymaking.entity.custom.EnderGhoulEntity;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -13,5 +17,15 @@ public class ModEvents {
   @SubscribeEvent
   public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
     event.put(ModEntities.ENDER_GHOUL.get(), EnderGhoulEntity.setAttributes());
+  }
+
+  @SubscribeEvent
+  public static void entitySpawnRestriction(SpawnPlacementRegisterEvent event) {
+    event.register(
+        ModEntities.ENDER_GHOUL.get(),
+        SpawnPlacements.Type.ON_GROUND,
+        Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+        Monster::checkMonsterSpawnRules,
+        SpawnPlacementRegisterEvent.Operation.REPLACE);
   }
 }
